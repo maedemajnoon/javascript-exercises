3c5c692cc45ec84f998a88da25a954727545547d
mahi mj <maede.majnoon96@gmail.com>
2025-01-10 18:26:49 +0330

    add simple and intermediate exercises

--- advanced/calculateAverage.js
+++ advanced/calculateAverage.js
@@ -9,5 +9,12 @@
  * calculateAverage([1, 2, 3, 4]) should return 2.5.
  * calculateAverage([10, 20, 30]) should return 20.
  */
-
+function calculateAverage(arr) {
+  let sum;
+  let avg;
+  arr.forEach((i) => {
+    sum += i;
+  });
+  return (avg = sum / arr.length);
+}
 module.exports = calculateAverage;
