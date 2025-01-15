31279750f5becbd799c7f54926f4f3efe2193b96
mahi mj <maede.majnoon96@gmail.com>
2025-01-16 02:11:02 +0330

    solved is palindrome

--- advanced/isPalindrome.js
+++ advanced/isPalindrome.js
@@ -11,5 +11,37 @@
  * isPalindrome(-121) should return false (negative numbers are not palindromes).
  * isPalindrome(10) should return false.
  */
-
+function isPalindrome(num) {
+  let numArray = num.toString().split("");
+  if (num < 0) return false;
+  if (num < 10) return true;
+  if (num < 100) {
+    if (numArray[0] === numArray[1]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  if (num < 1000) {
+    if (numArray[0] === numArray[2]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  if (num < 10000) {
+    if (numArray[0] === numArray[3] && numArray[1] === numArray[2]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  if (num < 100000) {
+    if (numArray[0] === numArray[4] && numArray[1] === numArray[3]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
 module.exports = isPalindrome;
