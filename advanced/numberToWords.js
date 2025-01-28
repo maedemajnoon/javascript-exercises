cd3ffc7e9b8d967d6596752426551d6102e0902b
mahi mj <maede.majnoon96@gmail.com>
2025-01-28 16:09:12 +0330

    added numberToWords

--- advanced/numberToWords.js
+++ advanced/numberToWords.js
@@ -11,6 +11,59 @@
  */
 
 // Write your solution here
-function numberToWords(num) {}
+function numberToWords(number) {
+  function recursive(num) {
+    if (num === 0) {
+      return "zero";
+    }
+    let teens = [
+      "",
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+      "ten",
+      "eleven",
+      "twelve",
+      "thirteen",
+      "fourteen",
+      "fifteen",
+      "sixteen",
+      "seventeen",
+      "eighteen",
+      "nineteen",
+    ];
+    let tens = [
+      "",
+      "",
+      "twenty",
+      "thirty",
+      "forty",
+      "fifty",
+      "sixty",
+      "seventy",
+      "eighty",
+      "ninety",
+    ];
+    if (num < 20) {
+      return teens[num];
+    }
+    if (num < 100) {
+      return tens[Math.floor(num / 10)] + " " + (num % 10)
+        ? ""
+        : teens[num % 10];
+    } else {
+      return teens[num % 100] + " hundred " + (num % 100)
+        ? ""
+        : recursive(num % 100);
+    }
+  }
+  recursive(number);
+}
 
 module.exports = numberToWords;
