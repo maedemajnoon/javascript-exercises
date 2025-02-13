fbe606e928e2bfe332bec7f07862154603c6e9f8
👨‍💻 Amir Dadipoor <46922127+amirdadipoor@users.noreply.github.com>
2025-02-13 12:17:35 +0330

    update average : modify and refactor average problem

--- advanced/calculateAverage.js
+++ advanced/calculateAverage.js
@@ -9,12 +9,5 @@
  * calculateAverage([1, 2, 3, 4]) should return 2.5.
  * calculateAverage([10, 20, 30]) should return 20.
  */
-function calculateAverage(arr) {
-  let sum = 0;
-  let avg = 0;
-  arr.forEach((i) => {
-    sum += i;
-  });
-  return (avg = sum / arr.length);
-}
+const calculateAverage = (arr) => arr.reduce( (sum , current) => sum + current , 0 ) / arr.length ;
 module.exports = calculateAverage;
