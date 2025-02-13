95d09d8cf58c4f677276114ec6c2317110e43de7
👨‍💻 Amir Dadipoor <46922127+amirdadipoor@users.noreply.github.com>
2025-02-13 13:19:53 +0330

    update sort : modify and refactor sort problem

--- advanced/sortArray.js
+++ advanced/sortArray.js
@@ -8,17 +8,5 @@
  * Example:
  * sortArray([3, 1, 4, 1, 5, 9]) should return [1, 1, 3, 4, 5, 9].
  */
-function sortArray(array) {
-  let temp;
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (array[j] > array[j + 1]) {
-        temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
-      }
-    }
-  }
-  return array;
-}
+const sortArray = (array) => array.sort((a, b) => a - b);
 module.exports = sortArray;
